0ad8a09e80f6b6289f9f85f5c278f4b98418d542
ncprakash <143497937+ncprakash@users.noreply.github.com>
2024-05-25 11:27:25 +0000

    dataTypes

---
+++ 01_basic/dataType.js
@@ -0,0 +1,35 @@
+// stack(primitive)  heap(non-primitive)
+// let nam="ncprakash";
+
+
+
+
+
+
+
+// console.log(nam)
+  //string 
+//   let places="mattikre to yeswantpura";
+//   let newString=new String(places);
+//   console.log(newString);
+  
+// console.log(newString.length);
+
+//   console.log(newString[2]);
+//   console.log(newString.__proto__);
+//   console.log(newString.toUpperCase());
+//   console.log(newString.charAt(3));
+//   console.log(newString.indexOf("9"));
+//   console.log(newString.substring(0,7));
+//   console.log(newString.slice(-4,0));
+//   console.log(newString.split(" "));
+
+
+
+  // 12.. Number and Maths in java script
+
+  const score=400;
+  console.log(score);
+  const balance =new Number(100)
+  console.log(balance);
+  console.log()
