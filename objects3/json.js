d6e12b6786056807f5567a5301ae8f33604cac46
ncprakash <143497937+ncprakash@users.noreply.github.com>
2024-06-22 14:55:22 +0000

    objects and json

---
+++ objects3/json.js
@@ -0,0 +1,20 @@
+//objects last part
+const course={
+    courseName:"js in hindi",
+    courseTeacher:"hitesh",
+    coursePrice:"1000"
+}
+// course.courseTeacher
+const{courseTeacher}=course;
+console.log(courseTeacher);
+const {courseTeacher:tt}=course;
+console.log(tt);
+
+//API
+//apna kam dusrsen sarpe dalna
+//json
+{
+"name":"hitesh",
+"coursename":"js in hindi",
+"price":"free"
+}
