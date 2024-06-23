60ae22c588911c344e60725b4d515c764fb2d644
ncprakash <143497937+ncprakash@users.noreply.github.com>
2024-06-23 08:27:20 +0000

    iifand arrow fucntion

--- 03_basic/iife.js
+++ 03_basic/iife.js
@@ -1,1 +1,12 @@
 // immediately invoked function expression (iife)
+console.log("uyufg");
+(function chai(){
+    console.log(`DB connected`)
+})();
+// chai()
+//iife syntax (function)(forexecution)
+
+
+((name)=>{
+    console.log("DB connected"+ name);
+})("ncprakash")
--- 03_basic/thisarrowfuction.js
+++ 03_basic/thisarrowfuction.js
@@ -5,9 +5,21 @@ const user={
         console.log(`${this.username} ,welcome to website`)
     }
 }
-user.welcomeMessege()
-console.log(this)
-function chai(){
-    console.log(this)
-}
-chai()
+// user.welcomeMessege()
+// console.log(this)
+// function chai(){
+//     console.log(this)
+// }
+// chai()
+// //arrow function
+// const chai=()=>{
+//     let username="hitesh",
+//     console.log(this)
+
+// }
+// //arrow function 
+// const addtwo=(num1.num2)=>{
+//     return num1+num2;
+
+// }
+// console.log(addtwo(3,4))
